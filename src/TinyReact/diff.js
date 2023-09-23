b2396262cc41ec0a0ba076af0e27bb40a2f79a68
Christine-Only <tsy17321289030@163.com>
2023-09-23 16:02:35 +0800

    feat: 处理节点类型不同的dom

--- src/TinyReact/diff.js
+++ src/TinyReact/diff.js
@@ -1,3 +1,4 @@
+import createDOMElement from "./createDOMElement"
 import mountElement from "./mountElement"
 import updateNodeElement from "./updateNodeElement"
 import updateTextNode from "./updateTextNode"
@@ -7,6 +8,12 @@ export default function diff(virtualDOM, container, oldDOM) {
   // 判断 旧节点 是否存在
   if (!oldDOM) {
     mountElement(virtualDOM, container)
+  } else if (virtualDOM.type !== oldVirtualDOM?.type) {
+    // 根据 Virtual DOM 创建真实 DOM 元素
+    const newElement = createDOMElement(virtualDOM)
+    // 用创建出来的真实 DOM 元素 替换旧的 DOM 元素
+    // oldDOM.parentNode.replaceChild(newElement, oldDOM)
+    container.replaceChild(newElement, oldDOM)
   } else if (virtualDOM.type === oldVirtualDOM?.type) {
     if (virtualDOM.type === 'text') {
       // 更新内容
@@ -15,6 +22,7 @@ export default function diff(virtualDOM, container, oldDOM) {
       // 更新元素节点属性
       updateNodeElement(oldDOM, virtualDOM.props, oldVirtualDOM.props)
     }
+    // 对比子节点
     virtualDOM.children.forEach((child, i) => {
       diff(child, oldDOM, oldDOM.childNodes[i])
     })
